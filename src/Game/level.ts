2f77563c6bc88e598283a6c91ce6b72326ba87b3
Daniela <danielapatriciabystrom@gmail.com>
2024-01-15 16:04:26 +0100

    Added some to draw-function in Level

--- src/Game/level.ts
+++ src/Game/level.ts
@@ -1,16 +1,19 @@
 class Level {
+  private entities: Entity[];
+  private currentLevel: number;
+  private score: number;
+  private time: Time;
 
-    // private currentLevel: number;
-    // private score: number;
-    // private time: Time;
-
-    constructor() {
-        // this.currentLevel = 0;
-        // this.score = 0;
-        // this.time = new Time();
+  constructor() {
+    this.entities = [];
+    this.currentLevel = 0;
+    this.score = 0;
+    this.time = new Time();
+  }
+  update() {}
+  draw() {
+    for (let entity of this.entities) {
+      entity.draw();
     }
-    update() {
-    }
-    draw() {
-    }
+  }
 }
