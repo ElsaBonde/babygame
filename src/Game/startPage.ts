03a55d82738cf3fe764a76479a20224c44c651f0
Elsa Bonde <elsasofiabonde@gmail.com>
2024-01-16 17:10:28 +0100

    made startbutton somehow shiny and interactive

--- src/Game/startPage.ts
+++ src/Game/startPage.ts
@@ -1,31 +1,56 @@
 class StartPage {
-  constructor() {}
+  private buttonColor: p5.Color;
+
+  constructor() {
+    this.buttonColor = color(150);
+  }
 
   draw() {
     background("#441F61");
     background(backgroundImg);
+    this.startButton();
   }
-/**
- * Create 'start button' to welcome page
- */
+
+  /**
+   * Create 'start button' to welcome page
+   */
   startButton() {
     push();
-    fill("#FFA3B1");
-    rect(345, 542, 255, 60, 20);
+    //ger glittereffekt till bakgrunden
+    fill(this.glitterColor(this.buttonColor));
+    rect(372.5, 542, 255, 60, 15);
     pop();
-    
+
     push();
     textSize(28);
-    textFont('VT323');
-    text("PRESS SPACE TO START", 360, 580);
+    fill("black");
+    textFont("VT323");
+    text("PRESS SPACE TO START", 388, 580);
     pop();
   }
-  
-    keyPressedStart() {
-    if ( keyCode === 32) {
-        if (game && game.getCurrentPage() === "start") {
-            game.changePage();
-        }
+
+  // Skapa en glittrande effekt på färgen
+  glitterColor(baseColor: p5.Color) {
+    const glitteredColor = color(baseColor);
+    const variance = 10; //bestämmer hur snabbt det ska "glittra"
+
+    //lägger till de olika värderna för färgerna
+    glitteredColor.setRed(255 + random(-variance, variance)); // Färg 1
+    glitteredColor.setGreen(192 + random(-variance, variance)); // Färg 2
+    glitteredColor.setBlue(203 + random(-variance, variance)); // Färg 3
+
+    return glitteredColor;
+  }
+
+  update() {
+    this.startButton();
+  }
+
+  keyPressedStart() {
+    if (keyCode === 32) {
+      if (game && game.getCurrentPage() === "start") {
+        game.changePage();
+      }
     }
   }
 }
