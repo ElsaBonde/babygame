e39ddf6a7f1e1861a4c7f676d7d645ea2b7c4a3f
Elsa Bonde <elsasofiabonde@gmail.com>
2024-01-16 16:02:06 +0100

    added button and background for startpage

--- src/Game/startPage.ts
+++ src/Game/startPage.ts
@@ -1,7 +1,31 @@
 class StartPage {
+  constructor() {}
+
+  draw() {
+    background("#441F61");
+    background(backgroundImg);
+  }
+/**
+ * Create 'start button' to welcome page
+ */
+  startButton() {
+    push();
+    fill("#FFA3B1");
+    rect(345, 542, 255, 60, 20);
+    pop();
     
-    constructor() {}
-        
-    draw() {
+    push();
+    textSize(28);
+    textFont('VT323');
+    text("PRESS SPACE TO START", 360, 580);
+    pop();
+  }
+  
+    keyPressedStart() {
+    if ( keyCode === 32) {
+        if (game && game.getCurrentPage() === "start") {
+            game.changePage();
+        }
     }
+  }
 }
