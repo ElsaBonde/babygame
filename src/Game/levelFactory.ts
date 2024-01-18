5c2a7d6bc7f1fb060b5fad5865c0e26600142a77
Ida Casperson <ida_casperson@hotmail.com>
2024-01-18 11:27:39 +0100

    change background picture
    Co-authored-by: sallymaria99 <sallymaria99@users.noreply.github.com>
    Co-authored-by: Elin <mypinkworld@users.noreply.github.com>

--- src/Game/levelFactory.ts
+++ src/Game/levelFactory.ts
@@ -1,25 +1,26 @@
 class LevelFactory {
+  // private numbersGridLevel1: number[][];
+  // private numbersGridLevel2: number[][];
+  public levelOne: p5.Image;
+  // private backgroundLevel2: p5.Image;
+  // private colorLevel1: p5.Color;
+  // private colorLevel2: p5.Color;
 
-    // private numbersGridLevel1: number[][];
-    // private numbersGridLevel2: number[][];
-    // private backgroundLevel1: p5.Image;
-    // private backgroundLevel2: p5.Image;
-    // private colorLevel1: p5.Color;
-    // private colorLevel2: p5.Color;
+  constructor() {
+    // this.numbersGridLevel1 = [];
+    // this.numbersGridLevel2 = [];
+    this.levelOne = new p5.Image(1000, 600);
+    // this.backgroundLevel2 = new p5.Image();
+    //  this.colorLevel1 = new p5.color();
+    // this.colorLevel2 = new p5.color();
+  }
 
-    constructor() {
-        
-        // this.numbersGridLevel1 = [];
-        // this.numbersGridLevel2 = [];
-        // this.backgroundLevel1 = new p5.Image();
-        // this.backgroundLevel2 = new p5.Image();
-        // this.colorLevel1 = new p5.color();
-        // this.colorLevel2 = new p5.color();
-    }
+  draw() {
+    background(levelOne);
+  }
 
-    public generateLevel(levelNumber: number) { // här kommer vi behöva lägga en if-sats som kollar vilken level som ska genereras
-        return new Level();
-        
-    }
-
+  public generateLevel(levelNumber: number) {
+    // här kommer vi behöva lägga en if-sats som kollar vilken level som ska genereras
+    return new Level();
+  }
 }
