f6fbd9b12f4d7367c294828116159a9b20e41dd9
Daniela <danielapatriciabystrom@gmail.com>
2024-01-20 13:59:26 +0100

    Fixed clock-bugg. Now showing in game

--- src/Entity/clock.ts
+++ src/Entity/clock.ts
@@ -4,6 +4,6 @@ class Clock extends Entity {
   constructor(size: number, x: number, y: number) {
     super(clockImg, size, x, y);
   }
-  freezeTime(time: Time) {}
-  draw() {}
+  // freezeTime(time: Time) {}
+  // draw() {}
 }
