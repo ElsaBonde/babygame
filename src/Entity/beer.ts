5d7e9a91eb094b04a2f96ab926647ed143b3a92f
Daniela <danielapatriciabystrom@gmail.com>
2024-01-22 15:52:30 +0100

    Restructured baby- and level-code by gathering checkCollision in one method under levels.ts
    Co-authored-by: Elsa Bonde <ElsaBonde@users.noreply.github.com>
    Co-authored-by: Ida Casperson <ida_casperson@hotmail.com>
    Co-authored-by: sallymaria99 <sallymaria99@users.noreply.github.com>
    Co-authored-by: Elin <mypinkworld@users.noreply.github.com>

--- src/Entity/beer.ts
+++ src/Entity/beer.ts
@@ -4,11 +4,6 @@ class Beer extends Entity {
   constructor(size: number, x: number, y: number) {
     super(beerImg, size, x, y);
   }
-
-  remove() {
-    this.x = -100;
-    this.y = -100;
-  }
   /* public goSlow() {}
   private spinAround() {} */
   /*   draw() {} */
--- src/Entity/clock.ts
+++ src/Entity/clock.ts
@@ -4,11 +4,6 @@ class Clock extends Entity {
   constructor(size: number, x: number, y: number) {
     super(clockImg, size, x, y);
   }
-
-  remove() {
-    this.x = -100;
-    this.y = -100;
-  }
   // freezeTime(time: Time) {}
   // draw() {}
 }
