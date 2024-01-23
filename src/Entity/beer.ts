83786f2adfa77aedad5360a2a98ef83e944aa0be
Elsa Bonde <elsasofiabonde@gmail.com>
2024-01-23 14:31:22 +0100

    changed goSlow to baby.ts and added a goslow method when the baby is taking a beer

--- src/Entity/beer.ts
+++ src/Entity/beer.ts
@@ -4,7 +4,5 @@ class Beer extends Entity {
   constructor(size: number, x: number, y: number) {
     super(beerImg, size, x, y);
   }
-  /* public goSlow() {}
-  private spinAround() {} */
-  /*   draw() {} */
+  //private spinAround() {} */
 }
