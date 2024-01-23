10ea1c604a6768fbd7e0af4fefd20584c5c7a2e5
sallymaria99 <sallymaria@live.se>
2024-01-23 22:13:15 +0100

    text in endOfGame. More to do here

--- src/Game/endOfGame.ts
+++ src/Game/endOfGame.ts
@@ -9,7 +9,16 @@ class EndOfGame {
     this.textButton = "PLAY AGAIN";
   }
   draw() {
+    push();
     background("#441F61");
     background(endOfGameImg);
+
+    textSize(100);
+    fill("#EA85DA");
+    textAlign(CENTER, CENTER);
+    text(this.text, width / 2, height / 2);
+    textFont("Roboto Mono");
+    textStyle("bold");
+    pop();
   }
 }
